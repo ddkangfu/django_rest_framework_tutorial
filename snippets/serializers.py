11c9d35e12824981529f84a3b503a6e59e19f014
ddkangfu <ddkangfu@gmail.com>
2015-01-09 23:51:46 +0800

    Use ModelSerializer instead of Serializer.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,21 +3,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
-    pk = serializers.Field()
-    title = serializers.CharField(reqired=False, max_length=100)
-    code = serializers.CharField(widgets.TextArea, max_length=100000)
-    linenos = serializers.BooleanField(required=True)
-    language = serializers.CharField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def restore_project(self, attrs, instance=None):
-        if instance:
-            instance.title = attrs.get('title', instance.title)
-            instance.code = attrs.get('code', instance.code)
-            instance.linenos = attrs.get('linenos', instance.linenos)
-            instance.language = attrs.get('language', instance.linenos)
-            instance.style = attrs.get('style', instance.style)
-            return instance
-
-        return Snippet(**attrs)
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
