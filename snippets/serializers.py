2305c43e8ebab4f540caf2e5dfb0071e250ff51a
ddkangfu <ddkangfu@gmail.com>
2015-01-10 22:57:53 +0800

    fix serializer error.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,5 +5,5 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Snippet
+        model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
