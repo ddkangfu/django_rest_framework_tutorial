b16103d8e4541a1c0945f1d513de606dc79e6812
ddkangfu <ddkangfu@gmail.com>
2015-01-10 22:53:20 +0800

    add views and urls.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,2 +1,6 @@
-from django.conf.urls import patterns, include, url 
+from django.conf.urls import patterns, url 
 
+urlpatterns = patterns('snippets.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+)
