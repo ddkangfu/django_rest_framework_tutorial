04f5c8e894e6acc47273c71b8e745529a08faa18
ddkangfu <ddkangfu@gmail.com>
2015-01-13 22:09:06 +0800

    add root function view.

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('snippets.views',
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^$', views.api_root),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,7 @@
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework import generics
 from rest_framework import permissions
 
@@ -37,3 +41,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
