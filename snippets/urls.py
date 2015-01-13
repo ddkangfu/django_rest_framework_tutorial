35fde576aa47ec6f6b515df90564dd7d2b43eacc
ddkangfu <ddkangfu@gmail.com>
2015-01-13 22:12:10 +0800

    add highlight view class.

--- snippets/urls.py
+++ snippets/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('snippets.views',
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^$', views.api_root),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
