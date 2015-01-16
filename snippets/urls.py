ca8e7b4405c6013a21a3ece79bb7571709d91dc1
ddkangfu <ddkangfu@gmail.com>
2015-01-17 00:03:40 +0800

    modify snippets.urls.

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,13 +2,38 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = patterns('snippets.views',
+from snippets.views import SnippetViewSet, UserViewSet
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+                                       'get': 'list',
+                                       'post': 'create'
+                                       })
+
+snippet_detail = SnippetViewSet.as_view({
+                                         'get': 'retrieve',
+                                         'put': 'update',
+                                         'patch': 'partial_update',
+                                         'delete': 'destroy'
+                                         })
+
+snippet_highlight = SnippetViewSet.as_view({
+                                            'get': 'highlight'
+                                            }, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+                                 'get': 'list'
+                                 })
+
+user_detail = UserViewSet.as_view({
+                                   'get': 'retrieve'
+                                   })
+
+urlpatterns = format_suffix_patterns(patterns('snippets.views',
     url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
     url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
     url(r'^$', views.api_root),
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-)
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+))
